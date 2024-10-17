2e46689f534391e4561f8351db325d456f193856
Vishnu Jangid <103036831+vj2303@users.noreply.github.com>
2024-10-17 17:24:44 +0530

    Update Services.jsx

--- src/app/partner-details/[id]/Services.jsx
+++ src/app/partner-details/[id]/Services.jsx
@@ -6,25 +6,26 @@ import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
 
 const ServicesSection = ({ services }) => {
-  const router = useRouter()
-  const { id } = useParams()
-    // const services = [
-    //     { id: 1, title: 'Hair-Cut, Wash & Style', image: '/img/icon.webp' },
-    //     { id: 2, title: 'Hair Colour', image: '/img/icon.webp' },
-    //     { id: 3, title: 'Nail Bar', image: '/img/icon.webp' },
-    //     { id: 4, title: 'Face', image: '/img/icon.webp' },
-    //     { id: 5, title: 'Hair-Cut, Wash & Style', image: '/img/icon.webp' },
-    //     { id: 6, title: 'Hair Colour', image: '/img/icon.webp' },
-    //     { id: 7, title: 'Nail Bar', image: '/img/icon.webp' },
-    //     { id: 8, title: 'Face', image: '/img/icon.webp' },
-    //   ];
+  const router = useRouter();
+  const { id } = useParams();
+
   return (
-    <div id="services" className="mt-3 sm:max-w-[1400px] mx-auto p-2 bg-white ">
+    <div id="services" className="mt-3 sm:max-w-[1400px] mx-auto p-2 bg-white">
       <h2 className="text-2xl sm:text-[32px] font-semibold mb-4">Services</h2>
-      <div className="grid grid-cols-3 sm:grid-cols-5 gap-2">
+      <div className="grid grid-cols-3 sm:grid-cols-5 gap-4">
         {services?.map((service) => (
-          <div key={service.id} className="text-center" onClick={()=>router.push(`/partner-details/${id}/service-details/service-${service.id}`)} >
-            <Image src={service.image_url} alt={service.name} width={100} height={100} className="h-20 sm:h-40 sm:w-40 w-20 rounded-lg mx-auto" />
+          <div
+            key={service.id}
+            className="text-center"
+            onClick={() => router.push(`/partner-details/${id}/service-details/service-${service.id}`)}
+          >
+            <Image
+              src={service.image_url}
+              alt={service.name}
+              width={160} // Fixed width in pixels
+              height={160} // Fixed height in pixels
+              className="rounded-lg mx-auto"
+            />
             <p className="text-[14px] sm:text-[18px] mt-2">{service.name}</p>
           </div>
         ))}
